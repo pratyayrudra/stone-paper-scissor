fff594ae56527b05e1ce337ad6eb7eb9cbd3a077
Pratyay Rudra <rudrapratyay1999@gmail.com>
2020-04-09 21:26:59 +0530

    PWA Added

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const static = "site-v1"
+const assets = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/index.js",
+    "/images/logo.png"
+]
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntil(
+        caches.open(static).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
