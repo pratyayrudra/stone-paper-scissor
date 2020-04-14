fb1ecb89693a4bff1e9032a385db5abe155aa2cd
Pratyay Rudra <rudrapratyay1999@gmail.com>
2020-04-15 00:00:53 +0530

    cache fixed

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,24 +1,35 @@
-const static = "site-v1"
+const cacheName = "site-v1"
 const assets = [
-    "/",
-    "/index.html",
-    "/style.css",
-    "/index.js",
-    "/images/logo.png"
+    "./",
+    "./index.html",
+    "./style.css",
+    "./index.js",
+    "./images/logo.png"
 ]
 
-self.addEventListener("install", installEvent => {
-    installEvent.waitUntil(
-        caches.open(static).then(cache => {
-            cache.addAll(assets)
+// Installing Service Worker
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            console.log('[Service Worker] Caching all: app shell and content');
+            return cache.addAll(assets);
         })
-    )
-})
+    );
+});
 
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then(res => {
-            return res || fetch(fetchEvent.request)
+// Fetching content using Service Worker
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            console.log('[Service Worker] Fetching resource: ' + e.request.url);
+            return r || fetch(e.request).then(function (response) {
+                return caches.open(cacheName).then(function (cache) {
+                    console.log('[Service Worker] Caching new resource: ' + e.request.url);
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
         })
-    )
-})
+    );
+});
