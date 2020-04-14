9ebd7526f7648ae6c9a5f203a1f67e1c15eb2cca
pratyayrudra <rudrapratyay1999@gmail.com>
2020-04-15 00:30:06 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -149,7 +149,7 @@ document.querySelector("#theme-switch").addEventListener("click", () => {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
     navigator.serviceWorker
-      .register(`${window.location.pathname}serviceWorker.js`)
+      .register("../../serviceWorker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err))
   })
